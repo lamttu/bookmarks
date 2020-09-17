3eafc86607b0b0662f564e77a5377f7b009cfe9e
lam-tu <lam.tu@xero.com>
2020-09-17 19:49:59 +1000

    Add middleware to all the response

---
+++ Bookmark.API/Middlewares/BrandMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace Bookmark.Middlewares
+{
+    public class BrandMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public BrandMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.Headers["copyright"] = "LamBookmarks2020";
+           
+            // Call the next delegate/middleware in the pipeline
+            await _next(context);
+        }
+    }
+}
---
+++ Bookmarks.UnitTests/BrandMiddlewareTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Bookmark.Middlewares;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.Hosting;
+using Moq;
+using Xunit;
+
+namespace Bookmarks.UnitTests
+{
+    public class BrandMiddlewareTests
+    {
+        [Fact]
+        public async Task BrandMiddleware_ShouldAddBrandForGetRequest()
+        {
+            using var host = await new HostBuilder()
+                .ConfigureWebHost(webBuilder =>
+                {
+                    webBuilder
+                        .UseTestServer()
+                        .Configure(app =>
+                        {
+                            app.UseMiddleware<BrandMiddleware>();
+                        });
+                })
+                .StartAsync();
+
+            var expectedBrand = "LamBookmarks2020";
+
+            var response = await host.GetTestClient().GetAsync($"/bookmarks/{Guid.NewGuid()}");
+
+            Assert.Contains(expectedBrand, response.Headers.GetValues("copyright"));
+        }
+    }
+}
